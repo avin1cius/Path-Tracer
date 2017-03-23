089627cbf2806a8581bec1928a009b129cdfa4ca
avin1cius <antoniovinicius97@gmail.com>
2017-03-23 18:52:11 -0300

    Fixes

--- raytracer.cpp
+++ raytracer.cpp
@@ -16,7 +16,7 @@ void RayTracer::integrate( void )
 {
     IntersectionRecord intersection_record;
 
-    glm::vec3 final_color;
+    glm::vec3 final_color{ 0.0f, 0.0f, 0.0f };
 
     // Image space origin (i.e. x = 0 and y = 0) at the top left corner.
 
@@ -37,22 +37,23 @@ void RayTracer::integrate( void )
         // Loops over image columns
         for ( std::size_t x = 0; x < buffer_.h_resolution_; x++ )
         {
-           intersection_record.t_ = std::numeric_limits< double >::max();
-
             for(std::size_t i = 0; i < samples_; i++ )
-            {
+            {   
+                intersection_record.t_ = std::numeric_limits< double >::max();
+
                 float rand1 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
                 float rand2 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 
-                Ray ray{ camera_.getWorldSpaceRay( glm::vec2{ x + 0.5f, y + 0.5f } ) };
-                //Ray ray{ camera_.getWorldSpaceRay( glm::vec2{ x + rand1, y + rand2 } ) };
+                //Ray ray{ camera_.getWorldSpaceRay( glm::vec2{ x + 0.5f, y + 0.5f } ) };
+                Ray ray{ camera_.getWorldSpaceRay( glm::vec2{ x + rand1, y + rand2 } ) };
 
                 if ( scene_.intersect( ray, intersection_record ) )
                     //buffer_.buffer_data_[x][y] = glm::vec3{ intersection_record.t_ * 0.2f };
-                    final_color = final_color + intersection_record.color_;
-
-                    buffer_.buffer_data_[x][y] = glm::vec3{ final_color / samples_ };
+                    final_color = final_color + intersection_record.color_;                    
             }
+
+            buffer_.buffer_data_[x][y] = glm::vec3{ final_color / static_cast <float> (samples_) };
+            final_color = { 0.0f, 0.0f, 0.0f };
         }
     }
 
